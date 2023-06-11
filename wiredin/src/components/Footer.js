6eef89e717f8f81e1cdac9790fc793bd9caaca18
70869 <etebong24@gmail.com>
2023-06-10 18:38:38 -0700

    Baseline

---
+++ wiredin/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2023 (wired-in). All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ wiredin/src/components/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', backgroundColor: 'black', color: 'white' }}>
+        <div>
+          <img src="/path/to/your/logo.png" alt="logo" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '50%' }} />
+          <h2 style={{ textAlign: 'center' }}>Community-focused brand providing high-performance game servers and secure file hosting services.</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;

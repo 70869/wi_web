c7afd11f325feadc59c3a4f5d8e3a5dabac288ff
70869 <etebong24@gmail.com>
2023-06-14 00:02:32 -0700

    MC Server Status
    niggas be trippin

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import bg from './images/bgblur.png'
 import Header from './Header';
 import picture from "./images/logo.png";
 import Footer from "./Footer";
+import axios from 'axios';
+
+function ServerStatus() {
+    const [status, setStatus] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://api.mcsrvstat.us/2/')
+            .then(response => {
+                setStatus(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
+
+    if (status === null || status.players === undefined) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div class="flex justify-between items-center bg-white dark:bg-white w-full p-4 mt-6">
+            <div class="text-xl font-bold">Test Environment</div>
+            <div>{status.players.online} players online</div>
+        </div>
+    );
+}
+
 
 function MainContent() {
     return (
@@ -16,8 +43,9 @@ function MainContent() {
                     <div class="overflow-hidden bg-transparent">
                         <div class="relative max-w-xl px-8 mx-auto sm:px-6 lg:max-w-7xl items-center">
                             <div>
-                                <h1 class="max-w-xl mx-auto text-start text-7xl font-bold tracking-tight text-gray-300 leading-normal">(wired-in)</h1>
-                                <p class="max-w-xl text-start text-gray-300 text-lg mb-4 tracking-wide">The next step for game servers and file hosting.</p>
+                            <h1 class="animate-fadein max-w-xl mx-auto text-start text-7xl font-bold tracking-tight text-gray-300 leading-normal">(wired-in)</h1>
+                            <p class="animate-fadein max-w-xl text-start text-gray-300 text-lg mb-4 tracking-wide">The next step for game servers and file hosting.</p>
+                            <ServerStatus />
                             </div>
                         </div>
                     </div>
@@ -30,7 +58,18 @@ function MainContent() {
                             <img class="mx-auto max-w-[35%] object-cover" src={picture} alt="placeholder" />
                             <div class="flex flex-col mx-auto">
                                 <h2 class="max-w-xl text-5xl font-extrabold tracking-tight text-center text-zinc-800 dark:text-zinc-200 leading-tight">About (wired-in)</h2>
-                                <p class="max-w-xl text-start mt-6 text-xl leading-6 text-zinc-600 dark:text-zinc-400">placeholder text.</p>
+                                <p class="max-w-xl text-start mt-6 text-xl leading-6 text-zinc-600 dark:text-zinc-400">
+                                 (wired-in) is a community-centered brand designed with gamers in mind. We are dedicated to providing top-tier, high-performance game servers, hosting popular titles like Minecraft, Counter-Strike, and many more. We are driven by our passion for gaming and the desire to create an unparalleled multiplayer experience.
+                                 <br/><br/>
+                                 But we're more than just game servers. Recognizing the need for secure, private storage solutions, we are proud to offer an exclusive, invite-only file hosting service. With end-to-end encryption, we're ensuring a secure space for all your important files.
+                                 <br/><br/>
+                                 Our mission is to bridge the gap between gaming and security, enhancing your digital experience. We're set to launch in the Fall of 2023, and we can't wait for you to join our community.
+                                 <br/><br/>
+                                 We operate on a tiered subscription model, which gives you the freedom to choose the level of service that suits your needs. Each tier offers different perks and advantages, from server access to increased storage space.
+                                 <br/><br/>
+                                 To solidify our community, we also offer (wired-in) branded merchandise. Show off your gamer spirit and become a part of our story.
+                                 <br/><br/>
+                                 At (wired-in), we're more than a brand - we're a community. Join us and become a part of our journey.</p>
                             </div>
                         </div>
                     </div>
@@ -39,6 +78,7 @@ function MainContent() {
                             <div class="flex flex-col mx-auto">
                                 <h2 class="max-w-2xl text-5xl font-extrabold tracking-tight text-center text-zinc-800 dark:text-zinc-200 leading-tight">High quality game servers</h2>
                                 <p class="max-w-xl text-start mt-6 text-xl leading-6 text-zinc-600 dark:text-zinc-400">yep.</p>
+                                <ServerStatus />    
                             </div>
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+      extend: {
+        keyframes: {
+          fadein: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' }
+          }
+        },
+        animation: {
+          fadein: 'fadein 2s ease-in forwards'
+        }
+      },
     },
     plugins: [],
-}
+  }
+  

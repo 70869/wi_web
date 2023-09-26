11a1b49d2be86a42403eed63d2169e9aa39ea7ff
333 <32320771+70869@users.noreply.github.com>
2023-09-26 00:50:13 +0000

    Update ServerStatus.js

--- src/ServerStatus.js
+++ src/ServerStatus.js
@@ -6,7 +6,7 @@ function ServerStatus() {
     const [status, setStatus] = useState(null);
 
     useEffect(() => {
-        axios.get('https://api.mcsrvstat.us/2/mc.hypixel.net')
+        axios.get('https://api.mcsrvstat.us/2/mc.wiredin.gg')
             .then(response => {
                 setStatus(response.data);
             })

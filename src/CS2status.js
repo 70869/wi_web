8bd6e643eabda9047cb4438644d6e1dbe3eeccca
bong <32320771+70869@users.noreply.github.com>
2023-11-18 17:21:27 +0000

    Update 3 files

--- src/CS2status.js
+++ src/CS2status.js
@@ -1,36 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import cs2Icon from './random-files/cs2.png';
 
-function CS2status({ apiKey, serverIP, appId }) {
-    const [status, setStatus] = useState(null);
-
-    useEffect(() => {
-        axios.get(`https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=${730}`)
-            .then(response => {
-                setStatus({ players: { online: response.data.response.player_count } });
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-                setStatus({ players: { online: 0 } });
-            });
-    }, [appId]);
-
-    const isOffline = status === null || status.players === undefined || status.players.online === undefined;
-
-
-    const statusText = isOffline ? 'Offline' : `${status.players.online} Online`;
-
-
-    return (
-        <div className="bg-opacity-30 bg-black text-white rounded-2xl backdrop-blur-md p-4 flex justify-between items-center">
-            <div className="flex items-center">
-                <img src={cs2Icon} alt="CS2 Icon" className="w-10 h-10" />
-                <p className="ml-2 text-xl font-bold">{serverIP}</p>
-            </div>
-            <p className="text-md font-bold text-customGreen" > in development </p>
-        </div>
-    );
+function CS2status({ serverIP }) {
+  return (
+    <div className="bg-opacity-30 bg-black text-white rounded-2xl backdrop-blur-md p-4 flex justify-between items-center">
+      <div className="flex items-center">
+      <img src={cs2Icon} alt="CS2 Icon" className="w-10 h-10" />
+        <p className="ml-2 text-xl font-bold">{serverIP}</p>
+        <p class="ml- text-xl font-bold text-customGreen">In active development.</p>
+      </div>
+      
+    </div>
+  );
 }
 
 export default CS2status;
--- src/ServerStatus.js
+++ src/ServerStatus.js
@@ -24,12 +24,12 @@ function ServerStatus() {
 
 
     return (
-        <div className="bg-opacity-30 bg-black text-white rounded-2xl backdrop-blur-md p-4 flex justify-between items-center">
-            <div className="flex items-center">
-                <img src={mcIcon} alt="MC Icon" className="w-10 h-10" />
-                <p className="ml-2 text-xl font-bold">mc.wiredin.gg</p>
+        <div class="bg-opacity-30 bg-black text-white rounded-2xl backdrop-blur-md p-4 flex justify-between items-center">
+            <div class="flex items-center">
+                <img src={mcIcon} alt="MC Icon" class="w-10 h-10" />
+                <p class="ml-2 text-xl font-bold">mc.wiredin.gg</p>
             </div>
-            <p className={`text-sm ${statusText === 'Offline' ? 'text-zinc-400' : 'text-emerald-500'}`}>{statusText}</p>
+            <p class={`text-sm ${statusText === 'Offline' ? 'text-zinc-400' : 'text-emerald-500'}`}>{statusText}</p>
         </div>
     );
 }

e41ac05c356b77853165297d04e13b24247ec8f7
70869 <32320771+70869@users.noreply.github.com>
2023-11-20 05:38:46 -0800

    The button is in pain..
    you will see...

--- src/ServerStatus.jsx
+++ src/ServerStatus.jsx
@@ -1,37 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import mcIcon from './images/mcicon.svg';
+import hitSound from './sounds/hitSound.mp3'; // Replace with the actual path to your sound file
 
 function ServerStatus() {
-    const [status, setStatus] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [hit, setHit] = useState(false);
 
-    useEffect(() => {
-        axios.get('https://api.mcsrvstat.us/2/mc.wiredin.gg')
-            .then(response => {
-                setStatus(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-                setStatus({ players: { online: 0 } });
-            });
-    }, []);
+  useEffect(() => {
+    axios.get('https://api.mcsrvstat.us/2/mc.wiredin.gg')
+      .then(response => {
+        setStatus(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+        setStatus({ players: { online: 0 } });
+      });
+  }, []);
 
-    // Check if status is null or players.online is undefined
-    const isOffline = status === null || status.players === undefined || status.players.online === undefined;
+  const isOffline = status === null || status.players === undefined || status.players.online === undefined;
+  const statusText = isOffline ? 'Offline' : `${status.players.online} Online`;
 
-    // Determine the status text and its color based on the server's online status
-    const statusText = isOffline ? 'Offline' : `${status.players.online} Online`;
+  const handleHit = () => {
+    setHit(true);
 
+    // Play the hit sound
+    const audio = new Audio(hitSound);
+    audio.play();
 
-    return (
-        <div class="bg-zinc-900 bg-opacity-75 backdrop-blur-sm text-white rounded-2xl p-4 flex justify-between items-center">
-            <div class="flex items-center">
-                <img src={mcIcon} alt="MC Icon" class="w-10 h-10" />
-                <p class="ml-2 text-xl font-bold">mc.wiredin.gg</p>
-            </div>
-            <p class={`text-sm ${statusText === 'Offline' ? 'text-zinc-400' : 'text-emerald-500'}`}>{statusText}</p>
+    setTimeout(() => {
+      setHit(false);
+    }, 500); // You can adjust the duration as needed
+  };
+
+  return (
+    <div
+      className={`hitable ${hit ? 'hit' : ''}`}
+      onClick={handleHit}
+    >
+      <div className={`bg-zinc-900 bg-opacity-75 backdrop-blur-sm text-white rounded-2xl p-4 flex justify-between items-center ${hit ? 'hit' : ''}`}>
+        <div className="flex items-center">
+          <img src={mcIcon} alt="MC Icon" className="w-10 h-10" />
+          <p className="ml-2 text-xl font-bold">mc.wiredin.gg</p>
         </div>
-    );
+        <p className={`text-sm ${statusText === 'Offline' ? 'text-zinc-400' : 'text-emerald-500'}`}>{statusText}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ServerStatus;

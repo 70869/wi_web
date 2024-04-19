bd4c714733e5324b099352c6ed068ae1b8be9b1d
bong <32320771+70869@users.noreply.github.com>
2024-04-19 10:05:59 -0700

    Update TeamContent.jsx

--- src/TeamContent.jsx
+++ src/TeamContent.jsx
@@ -36,7 +36,7 @@ function Team() {
                                 </div>
                             </div>
                             <div class="transition ease-in-out delay-75 hover:trasnlate-y-1 hover:scale-110 duration-300 flex bg-zinc-900 p-8 rounded-xl shadow-lg bg-opacity-75 backdrop-blur-sm">
-                                <img class="block mr-4 h-24 w-24 rounded-full" src="https://cdn.discordapp.com/avatars/257689851216789514/a_3e117317b87a141111c4b398ff674ca3.gif?size=2048&width=0&height=256" alt="soda"></img>
+                                <img class="block mr-4 h-24 w-24 rounded-" alt="new admin"></img>
                                 <div class="text-center space-y-2">
                                     <div class="space-y-0.5 text-left">
                                         <p class="text-lg md:text-2xl text-zinc-200 font-semibold">soda</p>
@@ -46,10 +46,11 @@ function Team() {
                                 </div>
                             </div>
                             <div class="transition ease-in-out delay-75 hover:trasnlate-y-1 hover:scale-110 duration-300 flex bg-zinc-900 p-8 rounded-xl shadow-lg bg-opacity-75 backdrop-blur-sm">
-                                <img class="block mr-4 h-24 w-24 rounded-full" src="https://cdn.discordapp.com/avatars/359199438759002112/f46ce7173faad7ef47d1e7201f810600.png?size=2048&width=0&height=256" alt="bobby jones"></img>
+                                <img class="block mr-4 h-24 w-24 rounded-full" src="
+                                    f46ce7173faad7ef47d1e7201f810600.png?size=2048&width=0&height=256" alt="bobby jones"></img>
                                 <div class="text-center space-y-2">
                                     <div class="space-y-0.5 text-left">
-                                        <p class="text-lg md:text-2xl text-zinc-200 font-semibold">Lyde</p>
+                                        <p class="text-lg md:text-2xl text-zinc-200 font-semibold">Justin</p>
                                         <p class="text-zinc-400 font-medium text-base md:text-lg">3D Artist</p>
                                     </div>
                                 </div>

5ab522d839232e40d6312562165419f793c676dd
bong <32320771+70869@users.noreply.github.com>
2024-05-05 15:45:46 -0700

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer class="flex flex-row justify-center text-center mx-auto py-4 text-zinc-700 dark:text-zinc-300 relative bg-white dark:bg-black">
-            <p>&copy; 2023 (wired-in). All rights reserved.</p>
+            <p>&copy; 2024 (wired-in). All rights reserved.</p>
         </footer>
     );
 }

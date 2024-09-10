8bf49105c5487b989b9c1b0539b8e04ed46e7e91
bong <32320771+70869@users.noreply.github.com>
2024-09-10 03:08:49 -0700

    TEST GIT PUSH I GUESS
    675t6786786789

--- src/TeamContent.jsx
+++ src/TeamContent.jsx
@@ -9,7 +9,6 @@ function Team() {
                     <div class="flex flex-col md:flex-row py-4 md:py-8">
                         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                             <div class="transition ease-in-out delay-75 hover:trasnlate-y-1 hover:scale-110 duration-300 flex bg-zinc-900 p-8 rounded-xl shadow-lg bg-opacity-75 backdrop-blur-sm" /** 1 */>
-                                        <p class="text-lg md:text-2xl text-zinc-200 font-semibold">Bong</p>
                                 <img class="block mr-4 h-24 w-24 rounded-full" src="https://avatars.githubusercontent.com/u/32320771?v=4" alt="etebong"></img>
                                 <div class="text-center space-y-2">
                                     <div class="space-y-0.5 text-left">
@@ -25,7 +24,7 @@ function Team() {
                                         <p class="text-lg md:text-2xl text-zinc-200 font-semibold">xxalem</p>
                                         <p class="text-zinc-400 font-medium text-base md:text-lg">Creative Director</p>
                                     </div>
-                                </div>
+                              movies  </div>
                             </div>
                             <div class="transition ease-in-out delay-75 hover:trasnlate-y-1 hover:scale-110 duration-300 flex bg-zinc-900 p-8 rounded-xl shadow-lg bg-opacity-75 backdrop-blur-sm" /** 3 */>
                                 <img class="block mr-4 h-24 w-24 rounded-full" src="https://avatars.githubusercontent.com/u/50128259?v=4" alt="gavin"></img>
@@ -37,7 +36,7 @@ function Team() {
                                 </div>
                             </div>
                             <div class="transition ease-in-out delay-75 hover:trasnlate-y-1 hover:scale-110 duration-300 flex bg-zinc-900 p-8 rounded-xl shadow-lg bg-opacity-70 backdrop-blur-sm">
-                                <img class="block mr-4 h-24 w-24 rounded-" alt="new admin"></img>
+                                <img class="block mr-4 h-24 w-24 rounded-" src="https://avatars.githubusercontent.com/u/7321764?v=4" alt="new admin"></img>
                                 <div class="text-center space-y-2">
                                     <div class="space-y-0.5 text-left">
                                         <p class="text-lg md:text-2xl text-zinc-200 font-semibold">soda</p>
@@ -47,8 +46,7 @@ function Team() {
                                 </div>
                             </div>
                             <div class="transition ease-in-out delay-75 hover:trasnlate-y-1 hover:scale-110 duration-300 flex bg-zinc-900 p-8 rounded-xl shadow-lg bg-opacity-70 backdrop-blur-sm">
-                                <img class="block mr-4 h-24 w-24 rounded-full" src="
-                                    f46ce7173faad7ef47d1e7201f810600.png?size=2048&width=0&height=256" alt="bobby jones"></img>
+                                <img class="block mr-4 h-24 w-24 rounded-full" src="https://cdn.discordapp.com/avatars/359199438759002112/f46ce7173faad7ef47d1e7201f810600.webp?size=80" alt="bobby jones"></img>
                                 <div class="text-center space-y-2">
                                     <div class="space-y-0.5 text-left">
                                         <p class="text-lg md:text-2xl text-zinc-200 font-semibold">Justin</p>
